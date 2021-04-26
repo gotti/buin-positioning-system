7f9e65e467da292ca2cfde818beddcc5aaa8e74d
gotti <18141824+gotti@users.noreply.github.com>
2021-04-27 01:32:15 +0900

    [bugfix][improve]

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 from bluepy import btle
-import threading
 import time
 import math
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+from scipy.stats import norm
 
 # 既知の受信機のUUIDとその座標，単位はcmであり部室の左PC側の端を原点とし廊下側がx正，右PC側がy正とする座標系
 receiversPositions = {
-        "fffe0215413980cd77bc09a261469f6451072392": [0,0],
-        "fffe0215c7f76b8b9e333abc5d4ba3538e0aa808": [0,290],
-        "fffe02150472b7bbce08788deb455a74c25cccce": [390,0]
+        "fffe0215c7f76b8b9e333abc5d4ba3538e0aa808": np.array([0,0]),
+        "fffe0215413980cd77bc09a261469f6451072392": np.array([0,290]),
+        "fffe02150472b7bbce08788deb455a74c25cccce": np.array([390,0])
         }
+zoom = 25
 
 class Receiver:
     uuid:str = ""
@@ -20,7 +21,7 @@ class Receiver:
     def __init__(self, uuid:str, rssi, time):
         self.uuid = uuid
         self.rssi = rssi
-        self.time = time
+        self.receivedTime = time
     def getUuid(self):
         return self.uuid
     def setRssi(self, rssi:int):
@@ -31,7 +32,7 @@ class Receiver:
         return receiversPositions[self.uuid]
 
 class MaxPosition:
-    maxPos = [0,0]
+    maxPos = np.array([0,0])
     maxDat = 0
     def __init__(self, maxPos, maxDat):
         self.maxPos = maxPos
@@ -44,27 +45,26 @@ class MaxPosition:
     def getMaxPos(self):
         return self.maxPos
 
+
 class User:
     receivedSignals = dict()
 
+def calc(p):
+    p[2] += calcNormalDistribution(calcEuclideanDistance(p[0:2], circle), radius, 50)
 
-# return distance from rssi1
-
-def calcDistance(rssi1:int, rssi2:int, distance:int):
-    return 1/(10**((-1/20)*(rssi2-(rssi1)))+1)*distance
-
-def calcEuclideanDistance(x1, y1, x2, y2):
-    return math.sqrt((x1-x2)**2+(y1-y2)**2)
+def calcEuclideanDistance(x,y):
+    return math.sqrt((x[0]-y[0])**2+(x[1]-y[1])**2)
 
 def calcNormalDistribution(d:float, mean:float, deviation:float):
-    return (1/math.sqrt(2*math.pi*deviation**2))*math.exp(-(d-mean)**2/(2*deviation**2))
-#grid = [[0] * 150 for i in range(90)]
-grid = np.zeros((150, 90))
+    return (1/math.sqrt(2*math.pi*(deviation**2)))*math.exp(-(d-mean)**2/(2*(deviation**2)))
+
+#grid = [[0] * 320 for i in range(420)]
+
 users = dict()
 
 scanner = btle.Scanner(0)
 while True:
-    detectedDevices = scanner.scan(6.0)
+    detectedDevices = scanner.scan(3.0)
     for detectedDevice in detectedDevices:
         for (adTypeCode, description, valueText) in detectedDevice.getScanData():
             if adTypeCode == 254:
@@ -80,44 +80,59 @@ while True:
                 users[rpid].receivedSignals[uuid] = Receiver(uuid, rssi, time.time())
                 print(users[rpid].receivedSignals[uuid])
                 print(users)
-    time.sleep(3)
-    for user in users.values():
-        for s in user.receivedSignals.values():
-            if time.time() - s.receivedTime() > 30:
-                print("deleted...")
-                del(s)
+    for uuid, user in users.items():
+        poplist = []
+        for rpid, s in user.receivedSignals.items():
+            if time.time() - s.receivedTime > 60:
+                poplist.append(rpid)
+        for p in poplist:
+            users[uuid].receivedSignals.pop(p)
+    for rpid, user in users.items():
+        print("user",rpid)
+        y = np.arange(int(900/zoom)).reshape(1,int(900/zoom)).repeat(int(1500/zoom),axis=0)
+        x = np.arange(int(1500/zoom)).reshape(int(1500/zoom),1).repeat(int(900/zoom),axis=1)
+        grid = np.zeros((int(1500/zoom),int(900/zoom)))
+        fig = plt.figure()
+        for uuid, s in user.receivedSignals.items():
+            print("-signal:",uuid[-6:])
         if len(user.receivedSignals) < 3:
             # 測位するには受信機の数が少ない
             continue
         for re1 in user.receivedSignals.values():
             for re2 in user.receivedSignals.values():
-                if re1.getPos() == re2.getPos():
+                if re1.uuid == re2.uuid:
+                    print("same receiver")
                     continue
-                hi = 10**((-1/20)*(re2.getRssi()-(re1.getRssi())))
-                if  hi > 1:
+                hi = 10**((-1/18)*(re1.getRssi()-(re2.getRssi())))
+                #print(hi)
+                if  hi >= 1:
                     continue
-                for x in range(-500, 1000, 10):
-                    for y in range(-300, 600, 10):
-                        if [x,y] == re1.getPos() or [x,y] == re2.getPos():
-                            print("continue")
-                            continue
-                        h = hi/(1-hi)
-                        i = hi/(1+hi)
-                        circlex = h*(re1.getPos()[0]-re2.getPos()[0])+re1.getPos()[0]
-                        circley = h*(re1.getPos()[1]-re2.getPos()[1])+re1.getPos()[1]
-                        gaisyux = i*(re2.getPos()[0]-re1.getPos()[0])+re1.getPos()[0]
-                        gaisyuy = i*(re2.getPos()[1]-re1.getPos()[1])+re1.getPos()[1]
-                        grid[int(x/10)+50][int(y/10)+30] += calcNormalDistribution(calcEuclideanDistance(x,y,circlex,circley),
-                                calcDistance(re1.getRssi(), re2.getRssi(), calcEuclideanDistance(gaisyux, gaisyuy, circlex, circley)),
-                                calcDistance(re1.getRssi(), re2.getRssi()-2, calcEuclideanDistance(gaisyux, gaisyuy, circlex, circley)) - calcDistance(re1.getRssi(), re2.getRssi(), calcEuclideanDistance(gaisyux, gaisyuy, circlex, circley)))
+                h = hi/(1-hi)
+                i = hi/(1+hi)
+                gaishu1 = h*(re1.getPos() - re2.getPos())+re1.getPos()
+                gaishu2 = i*(re2.getPos() - re1.getPos())+re1.getPos()
+                circle = (gaishu1+gaishu2)/2
+                radius = calcEuclideanDistance((gaishu1-gaishu2)/2,np.array([0,0]))
+                #print(circle,radius)
+                plt.annotate('', xy=np.dot(gaishu1,np.array([[0,1/zoom],[1/zoom,0]]))+np.array([300/zoom,500/zoom]), xytext=np.dot(gaishu2,np.array([[0,1/zoom],[1/zoom,0]]))+np.array([300/zoom,500/zoom]),
+                        arrowprops=dict(shrink=0, width=1, headwidth=8, 
+                                        headlength=10, connectionstyle='arc3',
+                                        facecolor='gray', edgecolor='gray')
+                       )
+                #np.apply_along_axis(calc, 1, grid)
+                d = np.sqrt(((x*zoom-500)-circle[0])**2+((y*zoom-300)-circle[1])**2)
+                grid += norm.pdf(d,radius,50)
+        maxPoint = MaxPosition(np.array([0,0]),0)
+        for x in range(-500, 1000, zoom):
+            for y in range(-300, 600, zoom):
+                if grid[int(x/zoom+500/zoom)][int(y/zoom+300/zoom)] > maxPoint.getMaxDat():
+                    maxPoint.setMax(np.array([x,y]), grid[int(x/zoom+500/zoom)][int(y/zoom+300/zoom)])
 
-        maxPoint = MaxPosition([0,0],0)
-        for x in range(-500, 1000, 10):
-            for y in range(-300, 600, 10):
-                if grid[int(x/10)+50][int(y/10)+30] > maxPoint.getMaxDat():
-                    maxPoint.setMax([x,y], grid[int(x/10)+50][int(y/10)+30])
-        fig = plt.figure()
         plt.imshow(grid)
         x,y = maxPoint.getMaxPos()
-        print((x/10)+50,(y/10)+30)
+        for i in receiversPositions.values():
+            plt.plot(int(i[1]/zoom)+(300/zoom), int(i[0]/zoom)+(500/zoom), marker='P',markersize=7, color='w')
+        plt.plot(int(y/zoom)+(300/zoom), int(x/zoom)+(500/zoom), marker='P',markersize=10, color='r')
+
+        print(x,y)
         fig.savefig("img.png")
